e274d645fd77f52a3de5cb016a57aaaa6a0e8593
Mostafa Essam <moustafa.essam@mimocodes.com>
2022-10-31 15:45:00 +0200

    fix provider

--- src/Providers/PaymentProvider.php
+++ src/Providers/PaymentProvider.php
@@ -12,7 +12,7 @@ class PaymentProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../views','payment');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        Artisan::call('make:model Callback',[
+        Artisan::call('php artisan make:model Callback',[
             '--class' => 'App\Models'
         ]);
     }
